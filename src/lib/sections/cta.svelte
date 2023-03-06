ba513afe4c3944c49fcfd1a68994a59970397c06
Sebastian <swebermorales@gmail.com>
2023-03-05 16:12:29 -0800

    added form for cta buttons

--- src/lib/sections/cta.svelte
+++ src/lib/sections/cta.svelte
@@ -8,6 +8,55 @@
   </p>
   <div class="flex gap-4 justify-center">
     <!-- <button class="btn btn-primary">Learn more</button> -->
-    <button class="btn btn-primary text-white">Get Started</button>
+    <!-- <button class="btn btn-primary text-white">Get Started</button> -->
+    <label for="my-modal" class="btn btn-primary text-white">Get started</label>
+  </div>
+</div>
+
+<input type="checkbox" id="my-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <label
+      for="my-modal"
+      class="btn btn-sm btn-primary btn-circle absolute right-2 top-2">✕</label
+    >
+    <form
+      class="flex flex-col gap-5"
+      name="email"
+      method="POST"
+      netlify-honeypot="nbot-field"
+      data-netlify="true"
+    >
+      <p class="self-center text-xl font-bold">Be part of the new internet.</p>
+      <input type="hidden" name="form-name" value="email" />
+      <input
+        name="name"
+        id="name"
+        class="input input-bordered w-full mt-10"
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        name="email"
+        id="email"
+        class="input input-bordered w-full"
+        type="text"
+        placeholder="example@gmail.com"
+      />
+      <textarea
+        name="details"
+        id="details"
+        rows="5"
+        class="textarea textarea-bordered w-full"
+        type="text"
+        placeholder="Any feedback would be greatly appreciated."
+      />
+      <input
+        for="my-modal"
+        type="submit"
+        class="btn btn-primary  text-white w-[50%] self-center"
+        value="Submit"
+      />
+    </form>
   </div>
 </div>
--- src/lib/sections/headerSection.svelte
+++ src/lib/sections/headerSection.svelte
@@ -11,12 +11,61 @@
         Powerful content algorithm, voting, moderation and monetization. Take
         charge of your feed - and finally control your mind.
       </p>
-      <button class="btn btn-primary text-white">Sign up</button>
+      <!-- <button class="btn btn-primary text-white">Sign up</button> -->
+      <label for="my-modal" class="btn btn-primary text-white">Sign up</label>
     </div>
     <div class="mockup-window border border-base-300">
       <div class="border-t border-base-300">
         <img src={Mockup2} alt="mockup" />
       </div>
     </div>
+  </div>
+</div>
+
+<input type="checkbox" id="my-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <label
+      for="my-modal"
+      class="btn btn-sm btn-primary btn-circle absolute right-2 top-2">✕</label
+    >
+    <form
+      class="flex flex-col gap-5"
+      name="email"
+      method="POST"
+      netlify-honeypot="nbot-field"
+      data-netlify="true"
+    >
+      <p class="self-center text-xl font-bold">Be part of the new internet.</p>
+      <input type="hidden" name="form-name" value="email" />
+      <input
+        name="name"
+        id="name"
+        class="input input-bordered w-full mt-10"
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        name="email"
+        id="email"
+        class="input input-bordered w-full"
+        type="text"
+        placeholder="example@gmail.com"
+      />
+      <textarea
+        name="details"
+        id="details"
+        rows="5"
+        class="textarea textarea-bordered w-full"
+        type="text"
+        placeholder="Any feedback would be greatly appreciated."
+      />
+      <input
+        for="my-modal"
+        type="submit"
+        class="btn btn-primary  text-white w-[50%] self-center"
+        value="Submit"
+      />
+    </form>
   </div>
 </div>
