20830e78f63c3f77d0fd28850224a684212e7e99
Sebastian <swebermorales@gmail.com>
2023-03-06 17:03:34 -0800

    footer and mockup cards responsiveness

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <footer
-  class="px-20 py-4 bg-base-100 text-base-content border-t-[1px] border-solid border-grey"
+  class=" px-5 md:px-20 py-4 bg-base-100 text-base-content border-t-[1px] border-solid border-grey"
 >
-  <div class="flex justify-between items-center">
+  <div class="flex flex-col md:justify-between items-center">
     <img src={CuatexLogo} alt="Logo" />
-    <p class="text-[#667085]">© 2023 Cuatex. All rights reserverd.</p>
+    <p class="text-[#667085] shrink">© 2023 Cuatex. All rights reserverd.</p>
   </div>
 </footer>
--- src/lib/components/mockupCards.svelte
+++ src/lib/components/mockupCards.svelte
@@ -5,49 +5,49 @@
 </script>
 
 <div class="flex flex-col items-center">
-  <h1 class="text-6xl font-bold">Own the platform</h1>
-  <p class="py-6 text-xl">
+  <h1 class="text-6xl font-bold text-center">Own the platform</h1>
+  <p class="py-6 px-5 text-xl text-center">
     We give you the tools to own your social media feed. Build the experience
     you want, own your data and avoid platform bias.
   </p>
 </div>
 {#each props as props, index}
   {#if index % 2 === 0}
-    <div class="bg-base-100 py-8">
-      <div class="lg:flex flex-row-reverse justify-between items-center">
-        <img class="basis-2/5" src={props.icon} alt={props.title} />
-        <div class="px-10">
-          <img class="mb-5" src={props.topIcon} alt={props.title} />
-          <h1 class="text-5xl font-bold">{props.title}</h1>
-          <p class="py-6 text-lg">
-            {props.content}
-          </p>
-          {#each props.iconDescription as description}
-            <div class="flex gap-4 items-center my-4">
-              <img src={CheckIcon} alt="Check Icon" />
-              <div class="text-lg">{description}</div>
-            </div>
-          {/each}
-        </div>
+    <div class="md:flex flex-row-reverse justify-between items-center">
+      <img class="basis-2/5" src={props.icon} alt={props.title} />
+      <div class="flex flex-col md:flex-none py-5 md:px-10">
+        <img class="mb-5 self-center" src={props.topIcon} alt={props.title} />
+        <h1 class="text-center md:text-left text-5xl font-bold">
+          {props.title}
+        </h1>
+        <p class="text-center md:text-left py-6 text-lg">
+          {props.content}
+        </p>
+        {#each props.iconDescription as description}
+          <div class="flex gap-4 items-center my-4 px-5">
+            <img src={CheckIcon} alt="Check Icon" />
+            <div class="text-lg">{description}</div>
+          </div>
+        {/each}
       </div>
     </div>
   {:else}
-    <div class="bg-base-100 py-8">
-      <div class="lg:flex  justify-between items-center">
-        <img class="basis-2/5" src={props.icon} alt={props.title} />
-        <div class="px-10">
-          <img class="mb-5" src={props.topIcon} alt={props.title} />
-          <h1 class="text-5xl font-bold">{props.title}</h1>
-          <p class="py-6 text-lg">
-            {props.content}
-          </p>
-          {#each props.iconDescription as description}
-            <div class="flex gap-4 items-center my-4">
-              <img src={CheckIcon} alt="Check Icon" />
-              <div class="text-lg">{description}</div>
-            </div>
-          {/each}
-        </div>
+    <div class="md:flex justify-between items-center">
+      <img class="basis-2/5" src={props.icon} alt={props.title} />
+      <div class="flex flex-col md:flex-none py-5 md:px-10">
+        <img class="mb-5 self-center" src={props.topIcon} alt={props.title} />
+        <h1 class="text-center md:text-left text-5xl font-bold">
+          {props.title}
+        </h1>
+        <p class="text-center md:text-left py-6 text-lg">
+          {props.content}
+        </p>
+        {#each props.iconDescription as description}
+          <div class="flex gap-4 items-center my-4 px-5">
+            <img src={CheckIcon} alt="Check Icon" />
+            <div class="text-lg">{description}</div>
+          </div>
+        {/each}
       </div>
     </div>
   {/if}
