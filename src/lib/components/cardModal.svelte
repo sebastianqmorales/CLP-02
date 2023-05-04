e2382eb64846012fc6d3af738344383cf78cd8db
Sebastian <swebermorales@gmail.com>
2023-05-04 14:40:52 -0700

    career cards

--- src/lib/components/cardModal.svelte
+++ src/lib/components/cardModal.svelte
@@ -1,67 +1,46 @@
-<div
-  class="card w-96 border-solid border-[1px] border-neutral drop-shadow-lg object-center"
->
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">Fullstack software engineer</h2>
-    <p>
-      We are looking for a Fullstack engineer that is passionate about their
-      industry.
-    </p>
-    <div class="card-actions justify-end">
-      <!-- <button class="btn btn-primary">Learn more</button> -->
-      <label
-        for="my-modal"
-        class=" border border-primary p-3 rounded-md bg-primary text-white text-sm cursor-pointer"
-        >Learn more</label
-      >
+<div class="flex flex-col">
+  <div
+    class="card w-96 border-solid border-[1px] border-neutral drop-shadow-lg self-center"
+  >
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Fullstack software engineer</h2>
+      <p>
+        We are looking for a Fullstack engineer that is passionate about their
+        industry.
+      </p>
+      <div class="card-actions justify-end">
+        <!-- <button class="btn btn-primary">Learn more</button> -->
+        <label
+          for="job-modal"
+          class=" border border-primary p-3 rounded-md bg-primary text-white text-sm cursor-pointer"
+          >Learn more</label
+        >
+      </div>
     </div>
   </div>
 </div>
 
-<input type="checkbox" id="my-modal" class="modal-toggle" />
+<input type="checkbox" id="job-modal" class="modal-toggle" />
 <div class="modal">
   <div class="modal-box">
     <label
-      for="my-modal"
+      for="job-modal"
       class="btn btn-sm btn-primary btn-circle absolute right-2 top-2">✕</label
     >
-    <form
-      class="flex flex-col gap-5"
-      name="email"
-      method="POST"
-      netlify-honeypot="nbot-field"
-      data-netlify="true"
-    >
-      <p class="self-center text-xl font-bold">Be part of the new internet.</p>
-      <input type="hidden" name="form-name" value="email" />
-      <input
-        name="name"
-        id="name"
-        class="input input-bordered w-full mt-10"
-        type="text"
-        placeholder="Name"
-      />
-      <input
-        name="email"
-        id="email"
-        class="input input-bordered w-full"
-        type="text"
-        placeholder="example@gmail.com"
-      />
-      <textarea
-        name="details"
-        id="details"
-        rows="5"
-        class="textarea textarea-bordered w-full"
-        type="text"
-        placeholder="Any feedback would be greatly appreciated."
-      />
-      <input
-        for="my-modal"
-        type="submit"
-        class="btn btn-primary text-white w-[50%] self-center"
-        value="Submit"
-      />
-    </form>
+    <div class="font-bold">What you will do:</div>
+    <ul class="list-disc">
+      <li>
+        Make important engineering decisions that have a long-lasting impact.
+      </li>
+      <li>
+        Building an entire app architecture from scratch with the co-founders.
+      </li>
+      <li>
+        Build reusable database wrappers for interacting with data in
+        JavaScript.
+      </li>
+      <li>Create UI for rendering the data in Svelte.</li>
+      <li>Develop data marketplace and data dignity.</li>
+    </ul>
   </div>
 </div>
