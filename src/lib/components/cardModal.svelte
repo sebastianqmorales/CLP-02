ed3baece9758f79aa7eca526d8bf922369a435ac
Sebastian <swebermorales@gmail.com>
2023-05-04 19:28:40 -0700

    completed careers card

--- src/lib/components/cardModal.svelte
+++ src/lib/components/cardModal.svelte
@@ -1,46 +1,110 @@
-<div class="flex flex-col">
-  <div
-    class="card w-96 border-solid border-[1px] border-neutral drop-shadow-lg self-center"
-  >
-    <div class="card-body items-center text-center">
-      <h2 class="card-title">Fullstack software engineer</h2>
-      <p>
-        We are looking for a Fullstack engineer that is passionate about their
-        industry.
-      </p>
-      <div class="card-actions justify-end">
-        <!-- <button class="btn btn-primary">Learn more</button> -->
-        <label
-          for="job-modal"
-          class=" border border-primary p-3 rounded-md bg-primary text-white text-sm cursor-pointer"
-          >Learn more</label
-        >
+<script>
+  import { each } from "svelte/internal";
+
+  export let props;
+</script>
+
+{#each props as props, index}
+  <div class="flex flex-col my-10">
+    <div
+      class="card w-96 border-solid border-[1px] border-[#fe7b43] drop-shadow-lg self-center"
+    >
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">{props.jobTitle}</h2>
+        <p>
+          {props.description}
+        </p>
+        <div class="card-actions justify-end">
+          <!-- <button class="btn btn-primary">Learn more</button> -->
+          <label
+            for={`job-modal-${index}`}
+            class=" border border-primary p-3 rounded-md bg-primary text-white text-sm cursor-pointer"
+            >Learn more</label
+          >
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<input type="checkbox" id="job-modal" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box">
-    <label
-      for="job-modal"
-      class="btn btn-sm btn-primary btn-circle absolute right-2 top-2">✕</label
-    >
-    <div class="font-bold">What you will do:</div>
-    <ul class="list-disc">
-      <li>
-        Make important engineering decisions that have a long-lasting impact.
-      </li>
-      <li>
-        Building an entire app architecture from scratch with the co-founders.
-      </li>
-      <li>
-        Build reusable database wrappers for interacting with data in
-        JavaScript.
-      </li>
-      <li>Create UI for rendering the data in Svelte.</li>
-      <li>Develop data marketplace and data dignity.</li>
-    </ul>
+  <input type="checkbox" id={`job-modal-${index}`} class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box flex flex-col gap-4">
+      <label
+        for={`job-modal-${index}`}
+        class="btn btn-sm btn-primary btn-circle absolute right-2 top-2"
+        >✕</label
+      >
+      <div class="jobTitle text-center text-xl font-semibold">
+        {props.jobTitle}
+      </div>
+      <div class="Cuatex-description text-center text-lg pt-4">
+        Cuatex is all about giving users control over their internet experience
+        and ending platform manipulation and bias. We want to bridge the gap
+        between data miners and users, giving users the chance to control their
+        data and earn their data dignity. Our vision is to democratize the
+        internet, increase participation in the internet economy, and empower
+        users to take responsibility for their online experience.
+      </div>
+      <div>
+        <div class="font-bold">Culture</div>
+        <ul class="list-disc pl-8">
+          <li>
+            Speak openly with the co-founders and be part of a transparent team.
+          </li>
+          <li>Encouraging and iterating ideas.</li>
+        </ul>
+      </div>
+      <div>
+        <div class="font-bold">What you will do</div>
+        <ul class="list-disc pl-8">
+          {#each props.jobWork as work}
+            <li>
+              {work}
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <div>
+        <div class="font-bold">What skills do you need?</div>
+        <ul class="list-disc pl-8">
+          {#each props.jobSkills as skill}
+            <li>
+              {skill}
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <div>
+        <div class="font-bold">Additional info</div>
+        {#each props.info as info}
+          <div>
+            {info}
+          </div>
+        {/each}
+      </div>
+      <div>
+        <div class="font-bold">Application</div>
+        <div class="description">
+          Please apply for this position by filling out the following form:
+        </div>
+        <a class="appLink" href={props.applicationLink}>
+          Click this link to fill out the application form.
+        </a>
+        <div class="description">
+          We will contact you soon if you fit the criteria for this role. Thank
+          you!
+        </div>
+      </div>
+    </div>
   </div>
-</div>
+{/each}
+
+<style>
+  .appLink {
+    color: #fe7b43;
+  }
+
+  .appLink:hover {
+    text-decoration: underline #fe7b43;
+  }
+</style>
--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,12 +1,65 @@
 <script>
   import LinkedInIcon from "$lib/assets/LinkedInIcon.svg";
-
   import CardModal from "../../lib/components/cardModal.svelte";
+
+  let props = [
+    {
+      jobTitle: "Fullstack Software Engineer",
+      description:
+        "We're Cuatex, a content platform that gives users complete control over algorithms, monetization, and moderation. We're currently looking for a few outstanding individuals to join our team and help us revolutionize the way we interact with the online world.",
+      jobWork: [
+        "Make important engineering decisions that have a long-lasting impact. ",
+        "Building an entire app architecture from scratch with the co-founders.",
+        "Build reusable database wrappers for interacting with data in JavaScript. ",
+        "Create UI for rendering the data in Svelte.",
+        "If you can, help develop microtransaction technology with Interledger Protocol",
+        "Develop data marketplace and data dignity",
+        "Have input on application UI",
+      ],
+      jobSkills: [
+        "Understanding how DB communicates with frontend.",
+        "Knowledge of databases, intermediate understanding of SQL, NoSQL, different use cases, databases (querying, schemas, etc.)",
+        "Developing Restful APIs.",
+        "In depth knowledge of JavaScript, CSS and HTML.",
+        "Frontend-frameworks (Svelte.js is preferred, but React, Angular, or Vue are also good).",
+        "NPM + Node (Express.js is a plus).",
+        "Software architecture, Git, GitHub, follow established code standards, able to read documentation.",
+      ],
+      info: [
+        "Job is part time remote, very flexible in both hours and what you are working on. Please apply regardless of your skill level.",
+      ],
+      applicationLink: "https://forms.office.com/r/8RCCZr588j",
+    },
+    {
+      jobTitle: "Marketing Specialist",
+      description:
+        "We're Cuatex, a content platform that gives users complete control over algorithms, monetization, and moderation. We're currently looking for a few outstanding individuals to join our team and help us revolutionize the way we interact with the online world.",
+      jobWork: [
+        "Help spread awareness about the problems that Cuatex is trying to solve.",
+        "Identify content creators and viewers who can most benefit from Cuatex’s value prop.",
+        "Write marketing messages to post in the comments of content creators’ posts.",
+        "Write posts about the problems that our platform solves.",
+        "Revise and expand the landing page to properly communicate the problems we’re trying to solve and Cuatex’s value prop.",
+        "Help develop the marketing strategy for MVP rollout.",
+      ],
+      jobSkills: [
+        "General marketing and communication skills.",
+        "Strong writing skills and attention to detail.",
+        "Comprehension of the social media platform landscape and the problems and customer pain points with current platforms (ads, black-box algorithms, demonetization, etc.).",
+        "Creative and innovative.",
+        "High emotional intelligence, ability to understand and empathize with potential customers.",
+      ],
+      info: [
+        "Job is part time remote, very flexible in both hours and what you are working on. Please apply regardless of your skill level.",
+      ],
+      applicationLink: "https://forms.office.com/r/t0qE7MSNpG",
+    },
+  ];
 </script>
 
-<CardModal />
+<CardModal {props} />
 
-<div class="hero min-h-screen bg-base-100 py-0 lg:py-10">
+<!-- <div class="hero min-h-screen bg-base-100 py-0 lg:py-10">
   <div class="hero-content text-center flex flex-col gap-12">
     <div class="max-w-6xl">
       <h1 class="text-6xl font-bold">Careers page coming soon</h1>
@@ -59,7 +112,7 @@
       </form>
     </div>
   </div>
-</div>
+</div> -->
 
 <style>
   .linkedin-link {
