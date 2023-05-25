107516eeb66c3f6b211019fec7f3e149a7667008
Sebastian <swebermorales@gmail.com>
2023-05-24 18:30:04 -0700

    inital commit

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -3,8 +3,66 @@
   import InstagramIcon from "$lib/assets/instagram.svg";
 </script>
 
-<div class="hero min-h-screen bg-base-100 py-0 lg:py-10">
-  <div class="flex flex-col items-center gap-10">
+<div class="flex flex-col items-center gap-10 py-20">
+  <h1 class="text-4xl md:text-6xl font-bold text-center">Our Mission</h1>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    Cuatex’s mission is to democratize the internet's monetization, moderation
+    and algorithms. We believe in this mission because of our core values of
+    democracy, transparency, equity and optimism.
+  </p>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    At Cuatex we use a direct democracy approach, where each user has voting
+    power to influence the direction of the platform. As a user you can vote on
+    censorship, new features, Data Union decisions and more. We are always
+    striving to have as much user input and responsibility as possible. We know
+    that the average answer from a mass group of people will most likely be the
+    best way forward. Lastly, we believe in a humanist approach to technology,
+    which means that we create technology that is for people, not for
+    organizations or AI.
+  </p>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    We are transparent as much as we can be, of course there are practical
+    limitations to ensure our IP is protected, and when we IPO, we will have
+    more restrictions. Nevertheless, we give complete access to the inner
+    workings of Cuatex through our Feed Builder and Data Union program.
+    Transparency is important to us because it keeps us responsible for our
+    actions and it normalizes honesty. When we are transparent in our corporate,
+    product and marketing decisions we tread carefully. If we never had this
+    responsibility to be transparent, we could make decisions that are
+    unethical. Cuatex has transparency baked into its product and corporate
+    structure.
+  </p>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    Equity to us means being free of bias, seeing through multiple perspectives
+    and everyone owns what they produce. Our freedom of bias is engineered,
+    meaning that the product itself is incapable of having platform bias. With
+    our Feed Builder and Data Union, we give complete control and
+    decision-making power to you. Our platform is created to get the most out of
+    every perspective and you actually own what you’ve created.
+  </p>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    We are optimistic. We believe in a good, caring future. And we are
+    developing technology that is optimistic of the human need to think better
+    and be responsible. We believe optimism is the best way to address the
+    hardships of life while also creating a way to move forward.
+  </p>
+  <p
+    class="text-[#475467] pt-3 sm:px-[30px] sm:text-lg lg:px-[450px] lg:text-2xl text-center"
+  >
+    This is who we are at Cuatex and what our product stands for, if this
+    message resonated with you, then please sign up for our early release.
+  </p>
+  <!-- <div class="flex flex-col items-center gap-10">
     <h1 class="text-6xl font-bold">Follow us on LinkedIn and Instagram</h1>
     <a href="https://www.linkedin.com/company/cuatex">
       <img class="w-[50px]" src={LinkedInIcon} alt="linked in icon" />
@@ -12,5 +70,5 @@
     <a href="https://www.instagram.com/cuatexmedia/">
       <img class="w-[50px]" src={InstagramIcon} alt="linked in icon" />
     </a>
-  </div>
+  </div> -->
 </div>
